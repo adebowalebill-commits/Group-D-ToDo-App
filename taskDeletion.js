3ee3d901fb864a4f4b929ae21a3f575b4f8230cd
akinbami-abosede1 <akinbamiabosedesera1@gmail.com>
2026-06-14 16:38:49 +0100

    Implement delete task logic and event listener

--- taskDeletion.js
+++ taskDeletion.js
@@ -2,4 +2,15 @@ import { deleteTask } from "./store.js";
 
 export function initTaskDeletion(taskList, renderTasks) {
 
+    taskList.addEventListener("click", (event) => {
+
+        if (!event.target.classList.contains("delete-btn")) return;
+
+        const id = event.target.dataset.id;
+
+        deleteTask(id);
+
+        renderTasks();
+    });
+
 }
