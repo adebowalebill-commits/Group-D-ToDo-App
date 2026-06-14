6d330ca5000cfd10d4b89cfa9f7116e0f724c80e
akinbami-abosede1 <akinbamiabosedesera1@gmail.com>
2026-06-14 16:37:30 +0100

    Create task deletion module structure

--- taskDeletion.js
+++ taskDeletion.js
@@ -2,15 +2,4 @@ import { deleteTask } from "./store.js";
 
 export function initTaskDeletion(taskList, renderTasks) {
 
-    taskList.addEventListener("click", (event) => {
-
-        if (!event.target.classList.contains("delete-btn")) return;
-
-        const id = event.target.dataset.id;
-
-        deleteTask(id);
-
-        renderTasks();
-    });
-
 }
